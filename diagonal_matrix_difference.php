38cea43df0e77d76c3097092160154e040b1d6f0
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-08-03 10:28:04 +0100

    Update comment

--- diagonal_matrix_difference.php
+++ diagonal_matrix_difference.php
@@ -5,7 +5,7 @@
  * 1 2 3
  * 4 5 6
  * 9 8 9
- * The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+ * The left-to-right diagonal = 1+5+9. The right to left diagonal = 3+5+9. Their absolute difference is [15-17] = 2.
  *
  * Function description
  * Complete the  function in the editor below.
